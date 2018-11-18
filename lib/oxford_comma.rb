bd4b2a0187aadef4db18aff6eb1eb012ccf37ac2
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 02:53:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,14 @@
 def oxford_comma(array)
   counter = 0
   my_array=[]
-  while counter < array.size-1
+  if array.size ==1
+    return (array[0].to_a).join
+  else
+    while counter < array.size-1
     my_array.push(array[counter])
     counter += 1
+    end
   end
-
   my_array.push("and #{array[-1]}")
   return my_array.join(", ")
 end
