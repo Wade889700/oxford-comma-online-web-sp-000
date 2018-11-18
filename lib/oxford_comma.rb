a594c95912b3ed2c2ea173d5f5a837f81de3da16
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 02:46:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     my_array.push(array[counter])
     counter += 1
   end
-  last_one = array[-1]
-  my_array.push["and #{last_one}"]
+  my_array.push["and #{array[-1]}"]
   return my_array.join(", ")
 end
